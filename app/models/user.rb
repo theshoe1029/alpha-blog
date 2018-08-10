9b8ed5e06e214763bf44a05cd669037643a83e67
adamschueller <aschueller@zoominternet.net>
2018-08-09 20:07:08 -0400

    Signup users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
-class User < ApplicationRecord
+class User < ActiveRecord::Base
   has_many :articles
   before_save {self.email = email.downcase}
   validates :username, presence: true,
@@ -8,4 +8,5 @@ class User < ApplicationRecord
   validates :email, presence: true,
             length: {maximum:105}, uniqueness:{case_sensitive:false},
             format: {with: VALID_EMAIL_REGEX}
+  has_secure_password
 end
