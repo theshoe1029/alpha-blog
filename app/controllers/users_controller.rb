dbfc80ca9d4a12eab770b7356ea2652fc37e56d2
adamschueller <aschueller@zoominternet.net>
2018-10-12 00:48:10 -0500

    added login, logout buttons

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update, :show]
 
   def new
     @user = User.new
@@ -14,12 +15,10 @@ class UsersController < ApplicationController
     end
   end
 
-  def edit
-    @user = User.find(params[:id])
+  def edit  
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
         flash[:success] = "Your account was updated successfully"
         redirect_to articles_path
@@ -29,7 +28,6 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
     @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
 
